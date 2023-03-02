b3d7f2cc44681cec990999246d2ba19e256408ac
ss2020r <a1093377@mail.nuk.edu.tw>
2023-03-02 23:34:16 +0800

    oop_week2

---
+++ A1093377_0224_1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class A1093377_0224_1{
+public static void main(String[] args) {
+System.out.println("請輸入一個整數");
+Scanner sc=new Scanner(System.in);
+int inport=sc.nextInt();
+if(inport%2==0){
+System.out.println("你輸入的整數是偶數="+inport);
+}else{
+System.out.println("你輸入的整數是奇數="+inport);
+}
+}
+
+}
---
+++ A1093377_0224_2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class A1093377_0224_2{
+ public static void main (String [] argv){
+ System.out.println("請輸入攝氏溫度值");
+Scanner scn=new Scanner(System.in);
+float c=scn.nextFloat();
+ System.out.println(c*9/5+32);
+
+
+
+
+ }
+}
