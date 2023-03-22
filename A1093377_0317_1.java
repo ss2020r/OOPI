5715eef23f942a2319c69c8a3e85793d730dc7d6
ss2020r <a1093377@mail.nuk.edu.tw>
2023-03-23 00:33:38 +0800

    Create A1093377_0317_1.java

---
+++ A1093377_0317_1.java
@@ -0,0 +1,29 @@
+   import java.util.Scanner;
+
+public class A1093377_0317_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("請輸入N的值：");
+        int n = scanner.nextInt();
+
+        System.out.print("請輸入M的值：");
+        int m = scanner.nextInt();
+
+        int[][] array = new int[n][m];
+
+        for (int i = 0; i <array.length; i++) {
+            for (int j = 0; j <array[i].length; j++) {
+                array[i][j] = (i+1)*(j+1);
+            }
+        }
+
+        System.out.println("陣列內容：");
+        for (int[] row : array) {
+            for (int element : row) {
+                System.out.print(element);
+            }
+            System.out.println();
+        }
+    }
+}
